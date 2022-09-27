dde4b9a155e0ac80c7ceb93cb7fbeaf6a576d2f4
sungmin306 <another0306@naver.com>
2022-09-27 16:29:38 +0900

    use instagram model and admin

--- askcommpany/instagram/admin.py
+++ askcommpany/instagram/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from . models import Post
 # Register your models here.
+admin.site.register(Post)
+
+#2번째 방법
+# class PostAdmin(admin,ModelAdmin):
+#     pass
+
+# admin.site.register(Post, PostAdmin)'
+
+#3번째 방법
+# @admin.regitser(Post) 장식자문법 -> Wrapping(감싸는 역할)
+# class PostAdmin(admin.ModelAdmin):
+#     pass
--- askcommpany/instagram/models.py
+++ askcommpany/instagram/models.py
@@ -5,3 +5,10 @@ class Post(models.Model):
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        # model 안에 있는 객체들의 이름을 post object 에서 해당 이름으로 변환 기능
+        #return f"custom post object({self.id})"
+        #return "custom post object({}).format(self.id)"
+        return self.message 
+    
