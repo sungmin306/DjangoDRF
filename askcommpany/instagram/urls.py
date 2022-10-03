6f866649a953e65cb019b6cc228d0c0f57a045c5
sungmin306 <another0306@naver.com>
2022-10-03 20:48:26 +0900

    add post_html

--- askcommpany/instagram/urls.py
+++ askcommpany/instagram/urls.py
@@ -1,7 +1,9 @@
 #urls.py까지 만드는데 app을 만드는 거라고 할 수 있다.
+from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.Post_list)
 
 ]
 
--- askcommpany/instagram/views.py
+++ askcommpany/instagram/views.py
@@ -1,3 +1,15 @@
+#from email import message
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def Post_list(request):
+    qs = Post.objects.all()
+    #print('전체 다 가져온 qs'+qs)
+    q=request.GET.get('q','')
+    if q: # q라는 검색어가 존재하면
+        qs = qs.filter(message__icontains=q) # 검색어가 존재하면 가져온다는 의미 icontains에 대해서 공부해보자
+        #print("qs는 " + qs)
+        #instagram/templates/instagram/post_list.html
+    return render(request, 'instagram/post_list.html',{
+        'post_list' : qs,
+    })
